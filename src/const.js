8fe8b4753594c1606af07cad7eef11926e24f0c4
Abinaya <abinaya.hashcodex@gmail.com>
2026-04-17 16:40:39 +0530

    changes file

---
+++ src/const.js
@@ -0,0 +1,18 @@
+import config from "./config/theme";
+
+export const constant = {
+    //live url
+    // backend_url: "https://api-jokkowallet.hashcodexperts.com/jokkoapi",
+    backend_url: "http://localhost:3700/jokkoapi",
+
+
+    adminRoute: config?.project?.name.split(" ")
+        .map((word, index) =>
+            index === 0
+                ? word.toLowerCase()
+                : word.charAt(0).toUpperCase() + word.slice(1)
+        )
+        .join(""),
+
+    // adminRoute: "",
+}
